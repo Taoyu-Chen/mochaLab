b4c036400b199fb8e9c1d2c13b468c01c60ea238
Taoyu Chen <875693150@qq.com>
2020-10-15 12:29:45 +0800

    Batch add - all use cases implemented and tested

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -32,11 +32,17 @@ class Catalogue {
       .map((p) => p.id);
     return result;
   }
-  batchAddProducts(batch) {
+ batchAddProducts(batch) {
+    const invalidAdditions = batch.products.filter(
+      (product) => this.findProductById(product.id) !== undefined
+    );
+    if (invalidAdditions.length > 0 ) {
+      throw new Error('Bad Batch')
+    }
     const validAdditions = batch.products.filter(
       (product) => product.quantityInStock > 0
-    )
-    validAdditions.forEach((p) => this.addProduct(p) );
+    );
+    validAdditions.forEach( (p) => this.addProduct(p) );
     return validAdditions.length;
   }
 }
